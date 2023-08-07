3666b42c74cc61c197427b8d915ae3cd60f3d1a6
unique-chan <yechankim@gm.gist.ac.kr>
2023-08-07 14:31:43 +0900

    + parse_args()

--- my_cfg/my_parser.py
+++ my_cfg/my_parser.py
@@ -15,9 +15,6 @@ class Parser:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='github.com/unique-chan/DBF')
         self.add_default_arguments()
-        # self.add_arguments('train')
-        # self.add_arguments('val')
-        # self.add_arguments('test')
 
     def add_default_arguments(self):
         self.parser.add_argument('--config_file', type=str,
@@ -26,18 +23,27 @@ class Parser:
                                  help='# samples per gpu')
         self.parser.add_argument('--gpu_ids', type=parse_list,
                                  help='gpu_ids e.g. "(1,3)" -> range(1,3) -> gpu id 1,2 will be used')
-        self.parser.add_argument('--device', default='cuda', type=str,
+        self.parser.add_argument('--device', default='cuda', choices=['cpu', 'cuda'], type=str,
                                  help='"cpu" or "cuda"?')
 
-    # def add_arguments(self, prefix):
-    #     self.parser.add_argument(f'--{prefix}_type', type=int,
-    #                              help=f'type of {prefix} dataset; if not specified, we will follow `dataset_type`')
-    #     self.parser.add_argument(f'--{prefix}_data_root', type=int,
-    #                              help=f'path of {prefix} dataset '
-    #                                   '(e.g. input/data_root/)')
-    #     self.parser.add_argument(f'--{prefix}_ann_file', type=str,
-    #                              help=f'path of annotation file for {prefix} dataset except for `train_data_root` '
-    #                                   f'(e.g. dataset/ImageSets/Main/{prefix}.txt)')
-    #     self.parser.add_argument(f'--{prefix}_img_prefix', type=str,
-    #                              help=f'prefix for all the {prefix} image samples '
-    #                                   '(e.g. dataset/)')
+    def parse_args(self):
+        return self.parser.parse_args()
+
+
+#######################################################################################################################
+# self.add_arguments('train')
+# self.add_arguments('val')
+# self.add_arguments('test')
+
+# def add_arguments(self, prefix):
+#     self.parser.add_argument(f'--{prefix}_type', type=int,
+#                              help=f'type of {prefix} dataset; if not specified, we will follow `dataset_type`')
+#     self.parser.add_argument(f'--{prefix}_data_root', type=int,
+#                              help=f'path of {prefix} dataset '
+#                                   '(e.g. input/data_root/)')
+#     self.parser.add_argument(f'--{prefix}_ann_file', type=str,
+#                              help=f'path of annotation file for {prefix} dataset except for `train_data_root` '
+#                                   f'(e.g. dataset/ImageSets/Main/{prefix}.txt)')
+#     self.parser.add_argument(f'--{prefix}_img_prefix', type=str,
+#                              help=f'prefix for all the {prefix} image samples '
+#                                   '(e.g. dataset/)')
