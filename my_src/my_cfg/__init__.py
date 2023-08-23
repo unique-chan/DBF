245fc0de075906c4848c4fd649367a89c4bf4d79
unique-chan <yechankim@gm.gist.ac.kr>
2023-08-23 21:22:14 +0900

    my_parser.py -> parser.py

--- my_src/my_cfg/__init__.py
+++ my_src/my_cfg/__init__.py
@@ -1,2 +1,8 @@
-from .my_parser import *
+import sys
+
+from .parser import *
 from .my_cfg_for_amod_v1 import *
+
+
+def get_config(parse_args):
+    return getattr(sys.modules[__name__], f'get_config_{parse_args.DATASET}')(parse_args)
