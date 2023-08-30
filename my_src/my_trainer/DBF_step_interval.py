df63164ef834b2a9d285f42cb7cb5ea4cf4e505b
unique-chan <yechankim@gm.gist.ac.kr>
2023-08-30 13:08:13 +0900

    wrong unlock condition changed

--- my_src/my_trainer/DBF_step_interval.py
+++ my_src/my_trainer/DBF_step_interval.py
@@ -4,7 +4,7 @@ def dynamic_backbone_freezing(runner):
     param = runner.param_for_dynamic_backbone_freezing
     assert (type(param) is dict
             and 'steps' in param.keys()), 'DBF_ARGS needed -> e.g. "%s"' % '{"steps": [1,10,20]}'
-    if epoch in param['steps'] == 0:
+    if epoch in param['steps']:
         model.bool_freeze_backbone = False  # unlock backbone
     else:
         model.bool_freeze_backbone = True   # lock backbone
