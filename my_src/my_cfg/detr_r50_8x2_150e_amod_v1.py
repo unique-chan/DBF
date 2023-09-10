a0accff72475276e1b97c0fd94fa96f6d97809f2
unique-chan <yechankim@gm.gist.ac.kr>
2023-09-10 21:14:01 +0900

    detr v1

---
+++ my_src/my_cfg/detr_r50_8x2_150e_amod_v1.py
@@ -0,0 +1,26 @@
+from mmcv import Config
+
+from ._base_cfg_common import get_base_ckpt_and_gpu_config, get_base_log_config
+from ._base_cfg_for_amod_v1 import get_base_data_config
+
+
+def get_config(parse_args, verbose=True):
+    PRETRAINED_MODEL_CONFIG = 'mmdetection/configs/detr/detr_r50_8x2_150e_coco.py'
+    cfg = Config.fromfile(PRETRAINED_MODEL_CONFIG)
+    cfg = get_base_data_config(cfg, parse_args)
+    cfg = get_base_ckpt_and_gpu_config(cfg, parse_args)
+    cfg = get_base_log_config(cfg)
+    # cfg.model.roi_head.bbox_head.num_classes = 13
+    cfg.model.bbox_head.num_classes = 13
+    cfg.optimizer.lr = 0.01
+    cfg.runner.max_epochs = parse_args.EPOCHS
+    cfg.lr_config = dict(
+        policy='step',
+        warmup=None,
+        warmup_iters=500,
+        warmup_ratio=0.001,
+        step=[100]
+    )
+    if verbose:
+        print(f'▶️ {cfg.pretty_text}')
+    return cfg
