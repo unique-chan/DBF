3a8a100c97c0f67b6064375bbc77fc53ef6e2bad
unique-chan <yechankim@gm.gist.ac.kr>
2023-11-22 02:09:14 +0900

    extend overwrite_base_models for obb

--- my_src/preliminaries/overwrite_base_models.py
+++ my_src/preliminaries/overwrite_base_models.py
@@ -4,14 +4,19 @@ from time import sleep
 
 # Run this code in the root directory: i.e. '/your_folders/.../DBF'
 
-root = 'mmdetection/mmdet/models/detectors'
-candidates = [f'single_stage.py', f'two_stage.py']
-for candidate in candidates:
-    if not os.path.exists(f'{root}/{candidate[:-3]} (original).py'):
-        os.rename(f'{root}/{candidate}', f'{root}/{candidate[:-3]} (original).py')
-        print(f'* Renamed: from "{root}/{candidate}" -> to "{root}/{candidate[:-3]} (original).py"')
-        sleep(0.1)
-        copy2(f'stages/{candidate}', f'{root}/{candidate}')
-        print(f'* Copied: from "stages/{candidate}" -> to "{root}/{candidate}"')
-    else:
-        print(f'* Already overwritten: "{candidate}" at "{root}"')
+roots = ['mmdetection/mmdet/models/detectors', 'mmrotate/mmrotate/models/detectors']
+stages = ['my_src/preliminaries/stages_for_hbb', 'my_src/preliminaries/stages_for_obb']
+
+assert len(roots) == len(stages)
+
+for i in range(len(roots)):
+    candidates = [f'single_stage.py', f'two_stage.py']
+    for candidate in candidates:
+        if not os.path.exists(f'{roots[i]}/{candidate[:-3]} (original).py'):
+            os.rename(f'{roots[i]}/{candidate}', f'{roots[i]}/{candidate[:-3]} (original).py')
+            print(f'* Renamed: from "{roots[i]}/{candidate}" -> to "{roots[i]}/{candidate[:-3]} (original).py"')
+            sleep(0.1)
+            copy2(f'{stages[i]}/{candidate}', f'{roots[i]}/{candidate}')
+            print(f'* Copied: from "stages/{candidate}" -> to "{roots[i]}/{candidate}"')
+        else:
+            print(f'* Already overwritten: "{candidate}" at "{roots[i]}"')
